9060bcf9bb7eac78bbad3e5631a840ccf50cf4ff
Ana Neagu <Ana.Neagu@theaccessgroup.com>
2022-04-26 11:37:56 +0300

    Added login

--- Shop app/config/routes.rb
+++ Shop app/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'baskets/show'
   get 'cards/show'
-  # get 'shops/index'
-  # get 'shops/show'
-  # resources :products
+
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   resources :products, only:[:index, :show, :new, :create]
   resources :order_items, only:[:create, :destroy, :update]
@@ -12,10 +14,7 @@ Rails.application.routes.draw do
   resource :baskets, only:[:show]
   resource :discount, only: :update
   resolve("Discount") { route_for(:discount) }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # resources :products
-  # Defines the root path route ("/")
-  # root "articles#index"
+ 
   root "products#index"
-  # get "/products", to: "products#show"
+ 
 end
