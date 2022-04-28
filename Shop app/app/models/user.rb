ba5d89c0c0c3bfecca102414502aee8654f0a168
Ana Neagu <Ana.Neagu@theaccessgroup.com>
2022-04-28 10:00:50 +0300

    Added Gravatar

--- Shop app/app/models/user.rb
+++ Shop app/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
-  # include Gravtastic
-  # gravtastic
+  include Gravtastic
+  gravtastic
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
