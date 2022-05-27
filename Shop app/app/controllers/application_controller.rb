a24d5edfda9afcd0303eeba8d1bb703b0f4f70fd
Ana Neagu <Ana.Neagu@theaccessgroup.com>
2022-05-27 17:35:32 +0300

    changes for vue access

--- Shop app/app/controllers/application_controller.rb
+++ Shop app/app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
     include ApplicationHelper
+      # protect_from_forgery with: :null_session
     add_flash_types :info, :error, :success
-    skip_before_action :verify_authenticity_token
-    before_action :authenticate_user!
-    # protect_from_forgery prepend: true
-    # protect_from_forgery with: :null_session
+
+    #commented for vue access
+    # skip_before_action :verify_authenticity_token
+    # before_action :authenticate_user!
+
+    #uncommented for vue access
+    protect_from_forgery prepend: true
     
 end
